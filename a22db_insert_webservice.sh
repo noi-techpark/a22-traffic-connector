2a43c5c80628153a483fff0a293dd42bc7fabc9c
henri <henri@moosbauer.com>
2023-06-01 15:14:42 +0200

    make reuse compliant

--- a22db_insert_webservice.sh
+++ a22db_insert_webservice.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+#
+# SPDX-License-Identifier: CC0-1.0
+
 # Create a22_webservice entry for local docker DB instance
 psql <<-END
     insert into a22.a22_webservice (id, url, username, password) values (1, '${A22_URL}', '${A22_USERNAME}', '${A22_PASSWORD}')
--- a22db_station_detail.sql
+++ a22db_station_detail.sql
@@ -1,3 +1,7 @@
+-- SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+--
+-- SPDX-License-Identifier: AGPL-3.0-or-later
+
 /*
     2022-11-24
     addition for full metadata support
