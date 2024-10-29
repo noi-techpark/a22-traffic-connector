7a827fedf2ed44291b532e44b69284f9a86c10af
Clemens Zagler <c.zagler@noi.bz.it>
2024-10-29 16:12:19 +0100

    fix typo in db migration script

--- a22db.migration.sql
+++ a22db.migration.sql
@@ -7,7 +7,7 @@ alter table a22.a22_traffic add license_plate_initials text;
 
 drop index if exists a22.a22_traffic_stationcode_ix;
 drop index if exists a22.a22_recent_data_ix_ccnew;
-CREATE INDEX a22_recent_data_ix_ccnew 
+CREATE INDEX a22_recent_data_ix 
 ON a22.a22_traffic USING btree (stationcode, "timestamp" DESC) WHERE ("timestamp" > 1728005677);
 drop index if exists a22.a22_recent2_data_ix;
 
